d8c7801d3fa230c959ac3ac1dfd3b12e61460b9b
Subhojeet Chowdhury <subhojeetchowdhury98@gmail.com>
2022-01-19 15:12:00 +0530

    scripting done

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 const bar = document.querySelector(".bar");
-
 const next = document.querySelector(".next");
 const previous = document.querySelector(".previous");
+const counters = document.querySelectorAll(".counter");
+
+let counter = 0;
+
+previous.addEventListener("click", regress);
+next.addEventListener("click", progress);
+
+function progress() {
+  counter++;
+  bar.style.width = `${counter * 0.333 * 600}px`;
+  counters[counter].style.borderColor = "#4780e9";
+  if (counter > 0) {
+    previous.classList.remove("disable");
+  }
+  if (counter === 3) {
+    next.classList.add("disable");
+  }
+}
+
+function regress() {
+  counter--;
+  bar.style.width = `${counter * 0.333 * 600}px`;
+  counters[counter + 1].style.borderColor = "#ccc";
+
+  if (counter < 3) {
+    next.classList.remove("disable");
+  }
+  if (counter === 0) {
+    previous.classList.add("disable");
+  }
+}
